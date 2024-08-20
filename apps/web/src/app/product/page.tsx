2093ed66b4396ec2db90f202f86c2e2d84a191b9
Bruno Wego <brunowego@gmail.com>
2024-08-19 23:38:19 -0300

    style: update return type to JSX.Element in page components

--- apps/web/src/app/product/page.tsx
+++ apps/web/src/app/product/page.tsx
@@ -1,3 +1,5 @@
-export default function ProductPage() {
+import type { JSX } from 'react'
+
+export default function ProductPage(): JSX.Element {
   return <h1 className='text-xl'>Product</h1>
 }
--- apps/web/src/app/sign-in/page.tsx
+++ apps/web/src/app/sign-in/page.tsx
@@ -1,3 +1,5 @@
-export default function SignInPage() {
+import type { JSX } from 'react'
+
+export default function SignInPage(): JSX.Element {
   return <h1 className='text-xl'>Sign In</h1>
 }
